b6c5e24798feafd4a962ef94668bf0151da97cd0
Mike Canoy <canoym@wwu.edu>
2017-12-05 14:07:53 -0800

    Finish Day 1 Part 1

--- y17/day01.py
+++ y17/day01.py
@@ -1,13 +1,26 @@
 """Day 1 - Advent of Code 2017"""
 
 
-def inverse_captcha():
-    """Day 1: Inverse Captcha
+def inverse_captcha(nums):
+    """Day 1: Inverse Captcha http://adventofcode.com/2017/day/1
 
-    "...find the sum of all digits that match the next digit in the list."
+    Args:
+        nums (str): String of digits.
 
-    http://adventofcode.com/2017/day/1
+    Returns:
+        sums (int): Sum of all digits that match the next digit in the string.
+
+    Raises:
+        None
     """
 
-if __name__ == '__main__':
-    inverse_captcha()
+    sums = 0
+
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i + 1]:
+            sums += int(nums[i])
+
+    if nums[-1] == nums[0]:
+        sums += int(nums[-1])
+
+    return sums
